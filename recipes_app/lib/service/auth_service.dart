6d349646cfbe0099d9c1c3c035c6ea174d16d547
John Ace Magno <127085144+jamydevelop@users.noreply.github.com>
2024-05-19 21:12:51 +0800

    Auth Login added and Added User JSON

--- recipes_app/lib/service/auth_service.dart
+++ recipes_app/lib/service/auth_service.dart
@@ -1,16 +1,29 @@
+import 'package:recipes_app/service/http_service.dart';
 
 class AuthService {
-  
   static final AuthService _singleton = AuthService._internal();
 
-  factory AuthService () {
+  final _httpService = HTTPService();
+
+  factory AuthService() {
     return _singleton;
   }
 
   AuthService._internal();
 
   Future<bool> login(String username, String password) async {
-    print(username);
+    try {
+      var response = await _httpService.post("auth/login", {
+        "username": username,
+        "password": password,
+      });
+      //print(response?.statusCode);
+      if (response?.statusCode == 200 && response?.data != null) {
+        print(response!.data);
+      }
+    } catch (e) {
+      print(e);
+    }
     return false;
   }
 }
--- recipes_app/lib/service/http_service.dart
+++ recipes_app/lib/service/http_service.dart
@@ -1,11 +1,43 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:recipes_app/const.dart';
 
 class HTTPService {
-  
   static final HTTPService _singleton = HTTPService._internal();
 
-  factory HTTPService () {
+  final _dio = Dio();
+
+  factory HTTPService() {
     return _singleton;
   }
 
-  HTTPService._internal();
+  HTTPService._internal() {
+    setup();
+  }
+
+  Future<void> setup({String? bearerToken}) async {
+    final headers = {
+      "Content-Type": "application/json",
+    };
+    final options = BaseOptions(
+        baseUrl: API_BASE_URL,
+        headers: headers,
+        validateStatus: (status) {
+          if (status == null) return false;
+          return status < 500;
+        });
+    _dio.options = options;
+  }
+
+  Future<Response?> post(String path, Map data) async {
+    try {
+      final response = await _dio.post(
+        path,
+        data: data,
+      );
+      return response;
+    } catch (e) {
+      print(e);
+    }
+  }
 }
